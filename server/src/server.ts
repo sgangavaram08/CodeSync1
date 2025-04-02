1a17cbb24ef9f68a3325e00ebcaf01cbc1b0920b
Gangavaram Sreevibha <123618407+sgangavaram@users.noreply.github.com>
2025-04-03 00:38:10 +0530

    Changes

--- server/src/server.ts
+++ server/src/server.ts
@@ -420,7 +420,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.get("/", (req: Request, res: Response) => {
   // Send the index.html file

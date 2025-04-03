ec5e9d48247504473fef1ebd6652251f4299d937
Gangavaram Sreevibha <123618407+sgangavaram@users.noreply.github.com>
2025-04-04 01:18:19 +0530

    Initial

---
+++ dist/assets/unist-util-visit-C5MGqtgf.js
@@ -0,0 +1,1 @@
+import{v as r}from"./unist-util-visit-parents-BC5uWo4u.js";function p(c,n,d,l){let e,t,i;typeof n=="function"?(t=void 0,i=n,e=d):(t=n,i=d,e=l),r(c,t,v,e);function v(o,u){const f=u[u.length-1],a=f?f.children.indexOf(o):void 0;return i(o,a,f)}}export{p as v};
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -19,11 +17,11 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes("node_modules")) {
+          if (id.includes('node_modules')) {
             return id
               .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
+              .split('node_modules/')[1]
+              .split('/')[0]
               .toString();
           }
         },
